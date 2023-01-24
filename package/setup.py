62f1bf7706014212546a633ef312511b276bfb4d
Lucas Hyunseung Kong <antibody91@naver.com>
2023-01-24 16:47:32 +0900

    init

---
+++ package/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='autotarget', # 패키지 명
+
+version='0.0.0.0',
+
+description='Disease-associated drug targets recommendation system using graph theory and machine learning',
+
+author='Hyunseung Kong',
+
+author_email='hskong@snu.ac.kr',
+
+url='https://github.com/gumgo91',
+
+license='MIT',
+
+py_modules=['autotarget'],
+
+python_requires='>=3',
+
+install_requires=[],
+
+packages=['autotarget']
+
+)
